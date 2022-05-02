d9d599bfcdd12d7339cb65ee73df3d8e70c181a3
nongsansanee <nong_com@hotmail.com>
2022-05-02 12:38:35 +0700

    edit API

--- app/APIs/HannahAPI.php
+++ app/APIs/HannahAPI.php
@@ -41,58 +41,6 @@ class HannahAPI
         return $data;
     
     }
-    public function getAdmission($an)
-    {
-        $headers = ['app' => config('app.HAN_API_SERVICE_SECRET'), 'token' => config('app.HAN_API_SERVICE_TOKEN')];
-        $options = ['timeout' => 8.0, 'verify' => false];
-        $url = config('app.HAN_API_SERVICE_URL').'admission';
-        $response = Http::withHeaders($headers)->withOptions($options)
-                         ->post($url, ['an' => $an]);
-        
-        $data = json_decode($response->getBody(),true);
-       
-        /*  error http  */
-        if($response->status()!=200){
-            return ['reply_code' => '1', 'reply_text' => 'request failed','found'=>'false'];
-        }
-        /*  not found AN */
-        if(!$data['found']) {
-            return ['reply_code' => '1', 'reply_text' => 'ไม่พบข้อมูลของเลข AN ที่ระบุมา','found'=>'false'];
-        }
-
-         /*   found AN  */
-        if (strlen($data['dob']) != 0) {
-            $dob = Carbon::createFromFormat('Y-m-d', $data['dob']);
-            $dateadmit = Carbon::createFromFormat('Y-m-d H:i:s', $data['admitted_at']);
-            $data['age'] = $dateadmit->diffInYears($dob);
-        } else {
-            $data['age'] = 'ไม่พบข้อมูลวันเกิด';
-        }
-        
-
-        if($data['gender']=='male')
-        {
-            $data['gender']=1;
-        }else{
-            $data['gender']=2;
-        }
-
-        /******show thai date ******/
-        $split_datetime_admit = explode(' ', $data['admitted_at']);
-        $split_date_admit = explode('-', $split_datetime_admit[0]);
-        $year = (int) $split_date_admit[0] + 543;
-        $thaimonth = ['', 'มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'];
-        $date_admit = $split_date_admit[2].'  '.$thaimonth[(int) $split_date_admit[1]].' '.$year;
-
-
-        $data['date_admit'] = $date_admit.' '.$split_datetime_admit[1];
-        $data['date_admit_save'] = $data['admitted_at']; 
-        $data['reply_code']=0;
-      
-        return $data;
-
-     
-    }
 
    
 }
